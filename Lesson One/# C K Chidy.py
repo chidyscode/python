7cffd5a71233069dea02ef5fa348b20eed6cba5c
chidyscode <111465136+chidyscode@users.noreply.github.com>
2022-08-23 15:12:53 +1200

    Add files via upload

---
+++ Lesson One/# C K Chidy.py
@@ -0,0 +1,18 @@
+# C K Chidy
+# Variables
+
+# Declaring my variables
+name = "Cheetah"
+numLegs = 4
+speed = 74.5
+fast = True
+# Output to screen
+print("One of the wild cats in the world, is called a",name,'.')
+print("An",name, "has",numLegs, "Legs.")
+print("It has an approximate speed of",speed,"metres per hour.")
+print("And, yes, it's",fast,".", "It's the fastest land animal.")
+
+# Replacing a variable value
+numLegs = 3
+print (numLegs)
+print (numLegs)
