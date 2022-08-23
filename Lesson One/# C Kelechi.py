7a68885cdd786b8687835daab6bff787bd10a307
chidyscode <111465136+chidyscode@users.noreply.github.com>
2022-08-23 14:48:33 +1200

    Add files via upload

---
+++ Lesson One/# C Kelechi.py
@@ -0,0 +1,14 @@
+# CK Kelechi
+# Learning Intention: Learning how to use variables
+
+# Declaring variables
+name = "Lion"
+numOfLegs = 4
+height = 1.2
+male = True
+
+# Output
+print(name)
+print(numOfLegs)
+print(height)
+print(male)
---
+++ Lesson One/variables.py
@@ -0,0 +1,7 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> numofSupercars = 60
+>>> 
+>>> print ("Bill Gates owns ", numofSupercars , " Super cars.")
+Bill Gates owns  60  Super cars.
+>>> 
