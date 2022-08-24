5e059c96dcfdc709e356997f7a66f4a9762bd1fe
chidyscode <111465136+chidyscode@users.noreply.github.com>
2022-08-25 09:58:15 +1200

    Add files via upload

---
+++ Lesson One/basic_var_output_labels.py
@@ -0,0 +1,17 @@
+# CK Kelechi
+# Learning Intention: Learning how to use variables
+
+# Declaring variables
+name = "cheetah"
+numOfLegs = 4
+height = 1.2
+male = True
+
+# Output variable type
+print(type(name))
+print(type(numOfLegs))
+print(type(height))
+print(type(male))
+
+
+print("A",name,"is the fastest land animal and it has",numOfLegs,"legs")
